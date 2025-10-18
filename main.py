dacf1f578572ce67f9130395b6ea8621e3ebd2b0
Valentin <valentin.jaffiol@gmail.com>
2025-10-18 19:57:13 +0200

    ajout midi to wav

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # server_convert.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file ,  jsonify
 import os
 import tempfile
 import numpy as np
 import librosa
 from mido import MidiFile, MidiTrack, Message
 import v2m
+import audio_tools.midi_to_wav , audio_tools.mixer
+
 
 app = Flask(__name__)
 
@@ -27,6 +29,17 @@ def get_next_midi_path(midi_dir=MIDI_DIR, prefix="output", ext=".mid"):
     next_idx = max(indices, default=0) + 1
     return os.path.join(midi_dir, f"{prefix}{next_idx}{ext}")
 
+
+def list_files_in_folder(folder_path):
+    """Retourne la liste des fichiers dans un dossier donné."""
+    if not os.path.exists(folder_path):
+        raise FileNotFoundError(f"❌ Le dossier '{folder_path}' n'existe pas.")
+    return [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+
+###################
+
+
 @app.route("/convert", methods=["POST"])
 def convert_wav_to_midi():
     if "file" not in request.files:
@@ -45,6 +58,36 @@ def convert_wav_to_midi():
 
     # Convertir WAV → MIDI
     v2m.convert_wav_to_midi(wav_path, midi_path)
+    
+    instru_name = "Trumpet"
+
+    #créer le wav du nouvel instru et stock son path 
+    new_instru_path = audio_tools.midi_to_wav.midi_to_wav(midi_path,"./GeneralUser-GS.sf2",instru_name,"./AUDIO")
+
+
+    file_path = "./AUDIO/band.wav"
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print(f"🗑️ Fichier supprimé avant regénération: {file_path}")
+
+
+    #trouve la liste des noms de fichiers dans le dossier audio
+    list_files_4_band = list_files_in_folder("./AUDIO")
+
+    #crée le mix band avec le nouvel instru et stock son path 
+    new_band_path = audio_tools.mixer.mix_wav_files("./AUDIO/band.wav",list_files_4_band,)
+
+
+    #zip des deux fichiers wav
+
+    #envoie les wav zippés
+    send_file()
+
+
+    #supprime le fichier band
+  
+
 
     return jsonify({"message": f"MIDI enregistre: {midi_path}"})
 
